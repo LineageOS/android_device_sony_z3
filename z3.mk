6a16fb8f5703e147ce6acba95261151e84c22b2e
tomascus <arbiter1000@gmail.com>
2017-07-26 10:20:29 +1000

    Revert "z3: Make audio configurations shinano common"
    This reverts commit 547d5f8872d5b5de1b7b7f5e21f2c9d0b5e43c45.
    
    Change-Id: I19c1eca93236a33bb3e972c142e8e8adab2becba

--- z3.mk
+++ z3.mk
@@ -41,6 +41,12 @@ TARGET_SCREEN_WIDTH := 1080
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
+# Audio configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
+
 # NFC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
