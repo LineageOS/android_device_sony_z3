b7890516072a65f7b813d128794766045a969920
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    z3: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-	 $(LOCAL_DIR)/lineage_z3.mk
+    $(LOCAL_DIR)/lineage_z3.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_z3-user \
+    lineage_z3-userdebug \
+    lineage_z3-eng
