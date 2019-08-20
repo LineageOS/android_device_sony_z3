6af90cc427bdddfe253669c278dbbb4b2959b48a
Arian <arian.kulmer@web.de>
2019-08-20 16:20:04 +0200

    z3: Convert system.prop to system_prop.mk

---
+++ system_prop.mk
@@ -0,0 +1,15 @@
+# Semc
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.semc.version.sw=1282-2729 \
+    ro.semc.version.cust=1288-7827 \
+    ro.semc.version.cust_revision=R4D \
+    ro.semc.product.model=D6603 \
+    ro.semc.ms_type_id=PM-0800-BV
+
+# LCD
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=480
+
+# USB path
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.usb.pid_suffix=1BA
